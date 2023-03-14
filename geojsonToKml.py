a13c5c25838c43f7a71a983fff7df524ff99cc5c
Benjaminthb <127867746+Benjaminthb@users.noreply.github.com>
2023-03-14 15:51:17 +0100

    Update geojsonToKml.py

--- geojsonToKml.py
+++ geojsonToKml.py
@@ -9,37 +9,6 @@ import os
 import json
 import simplekml
 
-# =============================================================================
-# # Ouvrir le fichier GeoJSON et le charger en tant que dictionnaire
-# with open("output.geojson") as f:
-#     data = json.load(f)
-# 
-# # Créer un nouvel objet KML
-# kml = simplekml.Kml()
-# 
-# 
-# # Parcourir chaque entité dans la collection de fonctionnalités GeoJSON
-# for feature in data["features"]:
-#     # Récupérer les propriétés et la géométrie de l'entité
-#     properties = feature["properties"]
-#     geometry = feature["geometry"]
-# 
-#     # Créer un nouvel objet de polygone pour représenter la géométrie
-#     polygon = kml.newpolygon()
-# 
-#     # Définir les propriétés du polygone
-#     polygon.name = f"Zone {properties['area']}"
-#     polygon.description = f"Date : {properties['date']}"
-#     polygon.outerboundaryis = geometry["coordinates"][0][0]
-# 
-# # for i in range(len(geometry["coordinates"][0][0])):
-# #     geometry["coordinates"][0][0][i][0], geometry["coordinates"][0][0][i][1] = geometry["coordinates"][0][0][i][1], geometry["coordinates"][0][0][i][0]
-# 
-# # Enregistrer le fichier KML
-# kml.save("output.kml")
-# 
-# =============================================================================
-
 
 # chemin du dossier contenant les fichiers
 path = "/home/bthoby/firefront-master/examples/aullene"
